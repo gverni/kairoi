e209d38bb1a903656551af52a7721aa470b18ff6
gverni <g.verni@gmail.com>
2019-03-17 16:01:19 +0000

    Added basic dependencies and configurations

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module'
+  },
+  rules: {
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+    'no-var': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    'quotes': ['error', 'single', { allowTemplateLiterals: true }]
+  },
+  globals: {
+  }
+}
---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const babel = require('gulp-babel')
+const eslint = require('gulp-eslint')
+const fsPromises = require('fs').promises
+
+const srcFiles = ['./src/**/*.js']
+
+gulp.task('clean', () => {
+  return fsPromises.readdir('dist')
+    .then(fileList => {
+      if (fileList.length > 0) {
+        let unlinkPromises = []
+        fileList.forEach(fileName => {
+          unlinkPromises.push(fsPromises.unlink(`dist/${fileName}`))
+        })
+        return Promise.all(unlinkPromises)
+      }
+    }).catch(error => {
+      if (error.code !== 'ENOENT') { return Promise.reject(error) }
+    })
+})
+
+gulp.task('build', () =>
+  gulp.src(srcFiles)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+  .pipe(gulp.dest('dist'))
+)
+
+gulp.task('lint', () => {
+  return gulp.src(srcFiles)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+})
+
+gulp.task('server', () => {
+  browserSync.init({
+    port: 8080,
+    notify: false,
+    reloadOnRestart: true,
+    https: false,
+    server: ['./']
+  })
+
+  gulp.watch([
+    '**/*.html',
+    '**/*.js'
+  ]).on('change', browserSync.reload)
+})
+
+gulp.task('development', gulp.series(
+  gulp.parallel('build'),
+  async function watch () {
+    gulp.watch(srcFiles, gulp.parallel('build'))
+  },
+  async function testServer () {
+    browserSync.init({
+      port: 8080,
+      notify: false,
+      reloadOnRestart: true,
+      https: false,
+      server: ['./'],
+      startPath: 'test/test.html'
+    })
+    gulp.watch([
+      'dist/*',
+      'test/*'
+    ]).on('change', browserSync.reload)
+  }
+))
