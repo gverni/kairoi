ace768da7f7f05487a342561728a039c67d80758
gverni <g.verni@gmail.com>
2019-03-18 16:15:04 +0000

    Added labella dependency in karma config

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       'node_modules/d3/dist/d3.js',
+      'node_modules/labella/dist/labella.js',
       'dist/*.js',
       'test/**/*.js'
     ],
