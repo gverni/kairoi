8128671f81002f2d680af5c30b8f25d3db49fcff
gverni <g.verni@gmail.com>
2019-03-28 17:11:52 +0000

    Adjusted test for new version of Kairoi

--- test/init.js
+++ test/init.js
@@ -11,27 +11,26 @@ QUnit.test('Init SVG', function (assert) {
   document.body.appendChild(timelineDiv)
   var testData = [
     {date: new Date('01 January 2019'), label: 'First milestone', color: 'green'},
-    {date: new Date('05 April 2019'), label: 'First milestone', color: 'green'},
-    {date: new Date('01 May 2019'), label: 'Fifth milestone', color: 'red'}
+    {date: new Date('05 April 2019'), label: 'Second milestone', color: 'blue'},
+    {date: new Date('01 May 2019'), label: 'Third milestone', color: 'red'}
   ]
-  var timeline = new Kairoi()
-  timeline.data = testData
-  timeline.target = '#timeline'
+  var timeline = new Kairoi('#timeline')
+  timeline.data(testData)
   timeline.draw()
   assert.ok(document.querySelector('#timeline > SVG'))
 })
 
 QUnit.test('Check axis range', function (assert) {
-  assert.ok(document.querySelectorAll('SVG .axis'))
-  let labels = document.querySelectorAll('SVG text')
+  let labels = document.querySelectorAll('SVG .axis-layer text')
   // Note that since we applied .nice() to the time scale, the date range is _rounded_
   // This may change in the future
+  assert.ok(document.querySelectorAll('SVG .axis-layer'))
   assert.equal(labels[0].innerHTML, 'Dec 30')
   assert.equal(labels[labels.length - 1].innerHTML, 'May 05')
 })
 
 QUnit.test('Check dots', function (assert) {
-  let dots = document.querySelectorAll('SVG .dot circle')
-  assert.equal(dots[0].attributes['cx'].value, '35.243135957657955')
-  assert.equal(dots[dots.length - 1].attributes['cx'].value, '949.5137280846841')
+  let dots = document.querySelectorAll('SVG .dot-layer circle')
+  assert.equal(dots[0].attributes['cx'].value, '18.101223949718822')
+  assert.equal(dots[dots.length - 1].attributes['cx'].value, '1103.7975521005624')
 })
